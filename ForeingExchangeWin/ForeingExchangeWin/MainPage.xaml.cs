a2582218f9a3c3af680824e36f49a0dfdba33a32
Otaske <luisgiraldo147581@correo.itm.edu.co>
2017-08-12 18:27:43 -0500

    Finish

---
+++ ForeingExchangeWin/ForeingExchangeWin/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ForeingExchangeWin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ConvertButton.Clicked += ConvertButton_Clicked;
+        }
+
+        void ConvertButton_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(PesosEntry.Text))
+            {
+                DisplayAlert("Error", "You must enter a value in pesos..", "Accept");
+                return;
+            }
+
+            decimal pesos = 0;
+            if (!decimal.TryParse(PesosEntry.Text, out pesos))
+            {
+                DisplayAlert("Error", "You must enter a value numeric in pesos..", "Accept");
+                PesosEntry.Text = null;
+                return;
+            }
+
+            var dollars = pesos / 2976.19048M;
+            var euros = pesos / 3517.96131M;
+            var pounds = pesos / 3848.21429M;
+
+            DollarsEntry.Text = string.Format("{0:C2}", dollars);
+            EurosEntry.Text = string.Format("{0:C2}", euros);
+            PoundsEntry.Text = string.Format("{0:C2}", pounds);
+        }
+    }
+}
